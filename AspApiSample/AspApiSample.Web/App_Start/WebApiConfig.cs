72f309901cdf03a97c45d05d30fd95af198b1c82
gritse <24323376+gritse@users.noreply.github.com>
2018-12-30 13:19:52 +0000

    change route

--- AspApiSample/AspApiSample.Web/App_Start/WebApiConfig.cs
+++ AspApiSample/AspApiSample.Web/App_Start/WebApiConfig.cs
@@ -16,8 +16,7 @@ namespace AspApiSample.Web
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "{controller}"
             );
         }
     }
